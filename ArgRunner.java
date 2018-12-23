8ab81776680a87eaf1cb7787eeae025c31ce730f
Konstantin Bryukhanov <kvbryukhanov@gmail.com>
2018-12-23 12:01:30 +0700

    add ArgRunner, InteractRunner

---
+++ ArgRunner.java
@@ -0,0 +1,19 @@
+/**
+	Класс для запуска калькулятора с параметрами
+*/
+
+public class ArgRunner {
+	
+	public static void main (String[] arg){
+		System.out.println("Calculate");
+		if (arg.length == 2) {
+			int first = Integer.valueOf(arg[0]);
+			int second = Integer.valueOf(arg[2]);
+			Calculator calc = new Calculator();
+			calc.add(first,second);
+			System.out.println("Sum : " + calc.getResult());
+		}else {
+			System.out.println("Enter two parameters.");
+		}
+	}
+}
--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,10 @@ public class Calculate {
 	
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
+		float first = Float.valueOf(arg[0]);
+		float second = Float.valueOf(arg[1]);
+		float devided = first/second;
+		System.out.println("Dev:  " + devided);
 	}
 	
 }
